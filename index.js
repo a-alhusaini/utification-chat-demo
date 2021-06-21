06a21e35dd0f6bc1c727af9f29bc0151756996af
Abdullah Alhusaini <arvg3a@gmail.com>
2021-06-21 09:02:33 +0300

    added push notification

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let express = require("express");
+let ServerSDK = require("utification-sdk/dist/server");
 let socket = require("socket.io");
+
+let serverSDK = new ServerSDK(
+  "df86eac4-8e13-4593-982f-10ed80971fb5",
+  "b393a7eb-c67a-4102-8a86-8b0f71c325d9"
+);
 
 let app = express();
 
@@ -14,5 +20,9 @@ let io = socket(server);
 io.on("connection", (socket) => {
   socket.on("chat", (data) => {
     io.sockets.emit("chat", data);
+    serverSDK.sendNotification(
+      {},
+      { title: "notification title", body: "notification body" }
+    );
   });
 });
